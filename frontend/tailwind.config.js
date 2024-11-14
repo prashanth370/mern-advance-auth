7024d6f0337133fbb125c87bd69823bb291ad1aa
prashanth370 <banothprashanth2002@gmail.com>
2024-11-14 20:29:19 +0530

    made changes one last time 1

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  plugins: [],
-}
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+})
