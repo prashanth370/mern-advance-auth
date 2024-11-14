7dd5c90611724a519d5a26525e942569f127e877
prashanth370 <banothprashanth2002@gmail.com>
2024-11-14 09:45:35 +0530

    Update build configuration and fix start script

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
       '@store': path.resolve(__dirname, './src/store'),
-      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
   build: {
